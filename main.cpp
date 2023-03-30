df9779c1a690bea4329794bd7c8b7cedb335ae64
galvardi <gal.vardi2@mail.huji.ac.il>
2023-03-30 11:52:37 +0300

    enable trap func

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@
 int main(){
     double one_operation_time = osm_operation_time(ITER);
     double empty_func_call_time= osm_function_time(ITER);
-//    double trap_time= osm_syscall_time(ITER);
+    double trap_time= osm_syscall_time(ITER);
 
     std::cout <<  "arithmetic op: " << one_operation_time << std::endl;
     std::cout << "empty func: " << empty_func_call_time << std::endl;
-//    std::cout << "trap: " << trap_time << std::endl;
+    std::cout << "trap: " << trap_time << std::endl;
 
     return 0;
 };
