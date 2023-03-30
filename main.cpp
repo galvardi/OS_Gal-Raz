5120cc99784ee6ccee6917db812587fb1551fb55
raz.zeevy <ra@LAPTOP-MHF4FA6R.localdomain>
2023-03-30 11:26:43 +0300

    magic numbers in main

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 //
 #include <iostream>
 #include "osm.h"
+#define ITER 1e8
 
 int main(){
-    double one_operation_time = osm_operation_time(1e8);
-    double empty_func_call_time= osm_function_time(1e8);
-//    double trap_time= osm_syscall_time(1e7);
+    double one_operation_time = osm_operation_time(ITER);
+    double empty_func_call_time= osm_function_time(ITER);
+//    double trap_time= osm_syscall_time(ITER);
 
     std::cout <<  "arithmetic op: " << one_operation_time << std::endl;
     std::cout << "empty func: " << empty_func_call_time << std::endl;
